bc12068e97c4f8c79d6416a6c8d4508554b02eb2
rasha08 <rasha08@gmail.com>
2020-09-16 09:23:06 +0200

    Updates

---
+++ precache-manifest.54a3e554e98a4e59962435fb35b1e8c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb333fc3719869238b9feb909fd74e29",
+    "url": "/reignn/index.html"
+  },
+  {
+    "revision": "bf3e06fc8d6eeabf78cb",
+    "url": "/reignn/static/css/4.50bf4612.chunk.css"
+  },
+  {
+    "revision": "e9aca61d9ec735ae3dd3",
+    "url": "/reignn/static/css/5.312891d4.chunk.css"
+  },
+  {
+    "revision": "61e83519d43193b8b2c6",
+    "url": "/reignn/static/css/main.bb85bd7a.chunk.css"
+  },
+  {
+    "revision": "0b43f961a9e05a3fe576",
+    "url": "/reignn/static/js/2.a3dfc409.chunk.js"
+  },
+  {
+    "revision": "8c4a52c548d60367d74557f7ce242617",
+    "url": "/reignn/static/js/2.a3dfc409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e544d65cae5b24bf10",
+    "url": "/reignn/static/js/3.e416016b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reignn/static/js/3.e416016b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3e06fc8d6eeabf78cb",
+    "url": "/reignn/static/js/4.1484e066.chunk.js"
+  },
+  {
+    "revision": "e9aca61d9ec735ae3dd3",
+    "url": "/reignn/static/js/5.f605ba98.chunk.js"
+  },
+  {
+    "revision": "61e83519d43193b8b2c6",
+    "url": "/reignn/static/js/main.c6107781.chunk.js"
+  },
+  {
+    "revision": "41404799a4ff070c8817",
+    "url": "/reignn/static/js/runtime-main.d12a461d.js"
+  }
+]);
